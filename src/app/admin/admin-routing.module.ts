7bd17c4742db8e969c82f7a1fe653975d5fd1fe2
Alexander Hodes <alexander.hodes@live.com>
2020-01-12 20:09:37 +0100

    added auth guard

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -3,12 +3,13 @@ import {RouterModule, Routes} from '@angular/router';
 import {ParkingspacesAdminComponent} from './components/parkingspaces-admin/parkingspaces-admin.component';
 import {UsersComponent} from './components/users/users.component';
 import {ProblemsComponent} from './components/problems/problems.component';
+import {AuthGuard} from '../shared/guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: ParkingspacesAdminComponent },
-  { path: 'parkingspaces', component: ParkingspacesAdminComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'problems', component: ProblemsComponent }
+  {path: '', component: ParkingspacesAdminComponent, canActivate: [AuthGuard]},
+  {path: 'parkingspaces', component: ParkingspacesAdminComponent, canActivate: [AuthGuard]},
+  {path: 'users', component: UsersComponent, canActivate: [AuthGuard]},
+  {path: 'problems', component: ProblemsComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
